7fc06f657f47db45f821ab0102c26956f1bc0fe0
egajunejhoice07 <145730038+egajunejhoice07@users.noreply.github.com>
2023-11-02 20:35:22 -0700

    Add files via upload

---
+++ Pertemuan2/RumusLuasLingkaran.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan2;
+
+/**
+ *
+ * @author pc15
+ */
+public class RumusLuasLingkaran {
+
+
+void luaslingkaran (double phi, double r){
+    double luas = phi*r*r;
+    System.out.println("Luas: "+luas);
+}
+}
